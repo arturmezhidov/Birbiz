da21983ae8aecbeef6d1e440b431b955d85a2f1d
Artur Mezhidov <arturrasulovich@gmail.com>
2016-12-15 13:19:44 +0300

    Created Birbiz.DataAccess.DataContracts project.

---
+++ src/Birbiz.DataAccess.DataContracts/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Birbiz.Common.Entities;
+
+namespace Birbiz.DataAccess.DataContracts
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Create(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        TEntity Delete(TEntity entity);
+
+        TEntity Delete(object entityId);
+
+        TEntity GetById(object entityId);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
---
+++ src/Birbiz.DataAccess.DataContracts/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Birbiz.Common.Entities;
+
+namespace Birbiz.DataAccess.DataContracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity;
+
+        void Save();
+    }
+}
