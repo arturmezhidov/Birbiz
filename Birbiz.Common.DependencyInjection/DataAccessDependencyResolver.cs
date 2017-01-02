13f0fb7c3e5e11fa95c748b4fcbdbbf8b18c7a73
Artur Mezhidov <arturrasulovich@gmail.com>
2017-01-02 13:30:08 +0300

    Created Birbiz.Common.DependencyInjection project and added DataAccessDependencyResolver.

---
+++ Birbiz.Common.DependencyInjection/DataAccessDependencyResolver.cs
@@ -0,0 +1,15 @@
+﻿using Birbiz.DataAccess.DataContracts;
+using Birbiz.DataAccess.SqlDataAccess;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Birbiz.Common.DependencyInjection
+{
+    public static class DataAccessDependencyResolver
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IUnitOfWork>(provider => new UnitOfWork(connectionString));
+            return services;
+        }
+    }
+}
