25ba2ca693a8e5ed91d9921811c2932a3324c9c7
Artur Mezhidov <arturrasulovich@gmail.com>
2017-03-04 02:47:53 +0300

    Create Navbar.

--- src/Birbiz.Presenter.WebUI/ClientApplication/src/app/shared/index.ts
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/shared/index.ts
@@ -1,5 +1,6 @@
 ﻿import { NgModule } from '@angular/core';
 
 import { AppbarModule } from './appbar/appbar.module';
+import { NavbarModule } from './navbar/navbar.module';
 
-export const SharedComponents = [AppbarModule];
+export const SharedComponents = [AppbarModule, NavbarModule];
---
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+import { NavbarItemComponent } from './navbar-item.component';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: './navbar.component.html'
+})
+export class NavbarComponent { }
