fb2db62836c4f0370d0272183bdd7a6d0d0adbaf
Artur Mezhidov <arturrasulovich@gmail.com>
2017-03-18 15:37:11 +0300

    Token support by http service.

--- src/Birbiz.Presenter.WebUI/ClientApplication/src/app/core/http/http.module.ts
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/core/http/http.module.ts
@@ -1,10 +1,11 @@
 ﻿import { NgModule } from '@angular/core';
 import { HttpModule as NgHttpModule } from '@angular/http';
 
+import { StoragesModule } from '../storages';
 import { HttpService } from './http.service';
 
 @NgModule({
-    imports: [NgHttpModule],
+    imports: [NgHttpModule, StoragesModule],
     providers: [HttpService]
 })
 export class HttpModule { }
--- src/Birbiz.Presenter.WebUI/ClientApplication/src/app/core/http/http.service.ts
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/core/http/http.service.ts
@@ -1,70 +1,126 @@
 ﻿import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-
 import { Http, Headers, Response, Request, RequestMethod, RequestOptions, RequestOptionsArgs } from '@angular/http';
+
+import { TokenStorage, Token} from '../storages';
 
 @Injectable()
 export class HttpService {
 
-    protected http: Http;
-    protected headers: Headers;
-    protected options: RequestOptions;
+    private http: Http;
+    private tokenStorage: TokenStorage;
 
-    constructor(http: Http) {
+    constructor(http: Http, tokenStorage: TokenStorage) {
         this.http = http;
-        this.headers = new Headers({
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        });
-        this.options = new RequestOptions({ headers: this.headers });
+        this.tokenStorage = tokenStorage;
     }
 
-    get<T>(url: string): Observable<T> {
-        let result: Observable<T> = this.query<T>(url, { body: '', method: RequestMethod.Get });
-        return result;
+    // GET
+
+    public get<T>(url: string, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, method: RequestMethod.Get }, options);
     }
 
-    post<T>(url: string, body: any): Observable<T> {
-        let result: Observable<T> = this.query<T>(url, { body: body, method: RequestMethod.Post });
-        return result;
+    public secureGet<T>(url: string, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, method: RequestMethod.Get }, options, true);
     }
 
-    put<T>(url: string, body: any): Observable<T> {
-        let result: Observable<T> = this.query<T>(url, { body: body, method: RequestMethod.Put });
-        return result;
+    // POST
+
+    public post<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Post }, options);
     }
 
-    delete<T>(url: string): Observable<T> {
-        let result: Observable<T> = this.query<T>(url, { body: '', method: RequestMethod.Delete } );
-        return result;
+    public securePost<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Post }, options, true);
     }
 
-    protected query<T>(url: string, requestArgs: RequestOptionsArgs): Observable<T> {
+    public postForm<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Post }, options, true, true);
+    }
 
+    // PUT
+
+    public put<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Put }, options);
+    }
+
+    public securePut<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Put }, options, true);
+    }
+
+    public putForm<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, body: body, method: RequestMethod.Put }, options, true, true);
+    }
+
+    // DELETE
+
+    public delete<T>(url: string, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, method: RequestMethod.Delete }, options);
+    }
+
+    public secureDelete<T>(url: string, options?: RequestOptionsArgs): Observable<T> {
+        return this.query<T>({ url: url, method: RequestMethod.Delete }, options, true);
+    }
+
+    // QUERY
+
+    public query<T>(requestArgs: RequestOptionsArgs, additionalOptions?: RequestOptionsArgs, isSecure?: boolean, isForm?: boolean): Observable<T> {
+
+        // create options
+        let defaultOptions: RequestOptions = this.createRequestOptions(isSecure, isForm);
+        let options: RequestOptions = this.mergeOptions(defaultOptions, requestArgs);
+        let requestOptionsArgs: RequestOptionsArgs = this.mergeOptions(options, additionalOptions);
+
+        // query
         let result: Observable<T> = this.http
-            .request(url, requestArgs)
-            .map(this.checkForError)
-            .catch(this.handleError)
+            .request(requestOptionsArgs.url, requestOptionsArgs)
             .map(this.toJson);
 
         return result;
     }
 
+    // Helpers
+
+    private createRequestOptions(isSecure?: boolean, isForm?: boolean): RequestOptions {
+
+        // headers
+        let headers = new Headers();
+
+        headers.append('Accept', 'application/json');
+
+        if (isSecure) {
+            headers.append('Authorization', 'Bearer ' + this.tokenStorage.getBearerToken());
+        }
+
+        if (isForm) {
+            headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        } else {
+            headers.append('Content-Type', 'application/json');
+        }
+
+        // request options
+        let options: RequestOptions = new RequestOptions();
+
+        options.headers = headers;
+ 
+        return options;
+    }
+
+    private mergeOptions(options: RequestOptions, optionsArgs: RequestOptionsArgs) {
+
+        if (options == null) {
+            options = new RequestOptions();
+        }
+
+        if (optionsArgs != null) {
+            options = options.merge(optionsArgs);
+        }
+
+        return options;
+    }
+
     private toJson(response: Response) {
         return response.json();
-    }
-
-    private handleError(error: any): Observable<Response> {
-        return Observable.throw(error);
-    }
-
-    private checkForError(response: Response): Response {
-        if (response.status >= 200 && response.status < 300) {
-            return response;
-        } else {
-            var error = new Error(response.statusText);
-            error['response'] = response;
-            throw error;
-        }
     }
 }
