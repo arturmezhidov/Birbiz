8abbae625e67310f1f26c8324d7f64caf29e2e67
Artur Mezhidov <arturrasulovich@gmail.com>
2017-03-31 23:47:32 +0300

    Add base success result.

--- src/Birbiz.WebServices.Auth/Results/RegisterResult.cs
+++ src/Birbiz.WebServices.Auth/Results/RegisterResult.cs
@@ -3,12 +3,8 @@ using Birbiz.WebServices.Common.Results;
 
 namespace Birbiz.WebServices.Auth.Results
 {
-    public class RegisterResult : BaseResult
+    public class RegisterResult : BaseSuccessResult
     {
-        public override bool IsSuccess { get { return true; } }
-
-        public override bool HasError { get { return false; } }
-
         public RegisterResult() : base((int)HttpStatusCode.OK)
         {
         }
---
+++ src/Birbiz.WebServices.Common/Results/BaseSuccessResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Birbiz.WebServices.Common.Results
+{
+    public class BaseSuccessResult : BaseResult
+    {
+        public override bool IsSuccess { get { return true; } }
+
+        public override bool HasError { get { return false; } }
+
+        public BaseSuccessResult(int statusCode) : base(statusCode)
+        {
+        }
+    }
+}
