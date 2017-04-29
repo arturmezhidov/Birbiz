1fd4294a557fc3b07bf30bd810f7fa88af00cbb2
Artur Mezhidov <arturrasulovich@gmail.com>
2017-04-29 21:35:06 +0300

    Added system colors.

---
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/config/system-colors.ts
@@ -0,0 +1,12 @@
+﻿export class SystemColors {
+    public static BG_HOME: string = "#FD1";
+    public static BG_CATALOG: string = "#009AD7";
+    public static BG_AUCTION: string = "#FFAA31";
+    public static BG_AUTOMOBILE: string = "#68AF27";
+    public static BG_REAL_ESTATE: string = "#C22439";
+    public static BG_FORUM: string = "#005683";
+    public static BG_BLOG: string = "#673499";
+    public static BG_NEWS: string = "#D53F26";
+    public static BG_DASHBOARD: string = "#43588A";
+    public static BG_ADMIN_DASHBOARD: string = "#43588A";
+}
--- src/Birbiz.Presenter.WebUI/ClientApplication/src/app/routes.ts
+++ src/Birbiz.Presenter.WebUI/ClientApplication/src/app/routes.ts
@@ -1,43 +1,71 @@
 ﻿import { Routes } from '@angular/router';
 
+import { SystemColors } from './config/system-colors';
 import { HomeContainer } from './modules/home/home.container';
 
 export const AppRoutes: Routes = [
     {
         path: '',
-        component: HomeContainer
+        component: HomeContainer,
+        data: {
+            backgroundColor: SystemColors.BG_HOME
+        }
     },
     {
         path: "catalog",
-        loadChildren: "es6-promise!./modules/catalog/catalog.module#CatalogModule"
+        loadChildren: "es6-promise!./modules/catalog/catalog.module#CatalogModule",
+        data: {
+            backgroundColor: SystemColors.BG_CATALOG
+        }
     },
     {
         path: "auction",
-        loadChildren: "es6-promise!./modules/auction/auction.module#AuctionModule"
+        loadChildren: "es6-promise!./modules/auction/auction.module#AuctionModule",
+        data: {
+            backgroundColor: SystemColors.BG_AUCTION
+        }
     },
     {
         path: "automobile",
-        loadChildren: "es6-promise!./modules/automobile/automobile.module#AutomobileModule"
+        loadChildren: "es6-promise!./modules/automobile/automobile.module#AutomobileModule",
+        data: {
+            backgroundColor: SystemColors.BG_AUTOMOBILE
+        }
     },
     {
         path: "real-estate",
-        loadChildren: "es6-promise!./modules/real-estate/real-estate.module#RealEstateModule"
+        loadChildren: "es6-promise!./modules/real-estate/real-estate.module#RealEstateModule",
+        data: {
+            backgroundColor: SystemColors.BG_REAL_ESTATE
+        }
     },
     {
         path: "forum",
-        loadChildren: "es6-promise!./modules/forum/forum.module#ForumModule"
+        loadChildren: "es6-promise!./modules/forum/forum.module#ForumModule",
+        data: {
+            backgroundColor: SystemColors.BG_FORUM
+        }
     },
     {
         path: "blog",
-        loadChildren: "es6-promise!./modules/blog/blog.module#BlogModule"
+        loadChildren: "es6-promise!./modules/blog/blog.module#BlogModule",
+        data: {
+            backgroundColor: SystemColors.BG_BLOG
+        }
     },
     {
         path: "news",
-        loadChildren: "es6-promise!./modules/news/news.module#NewsModule"
+        loadChildren: "es6-promise!./modules/news/news.module#NewsModule",
+        data: {
+            backgroundColor: SystemColors.BG_NEWS
+        }
     },
     {
         path: "dashboard",
-        loadChildren: "es6-promise!./modules/dashboard/dashboard.module#DashboardModule"
+        loadChildren: "es6-promise!./modules/dashboard/dashboard.module#DashboardModule",
+        data: {
+            backgroundColor: SystemColors.BG_DASHBOARD
+        }
     },
     {
         path: '**',
